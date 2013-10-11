a64eb0e3963b0f1f316e5f29dc23094eebd8ade2
sluu99 <sluu99@gmail.com>
2013-10-10 22:48:51 -0500

    Lesson 1 exercise solution

---
+++ code/01/Lesson1Average/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson1Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int num1;
+            int num2;
+            int num3;
+            double average;
+
+            Console.Write("Enter the first number: ");
+            userInput = Console.ReadLine();
+            num1 = int.Parse(userInput);
+
+            Console.Write("Enter the second number: ");
+            userInput = Console.ReadLine();
+            num2 = int.Parse(userInput);
+
+            Console.Write("Enter the third number: ");
+            userInput = Console.ReadLine();
+            num3 = int.Parse(userInput);
+
+            average = (num1 + num2 + num3) / 3.0D;
+            Console.WriteLine("Average of {0}, {1}, and {2} is {3}", num1, num2, num3, average);
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ code/01/Lesson1ChristmasTree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson1ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine(" *******");
+            Console.WriteLine("*********");
+            Console.WriteLine("    *");
+
+            Console.ReadKey(true);
+        }
+    }
+}

4ee9658eb26a2b9ec47461cfd13a11630199752b
Steven Luu <steven@luu.io>
2014-03-02 18:25:02 -0800

    Lesson3 code example

---
+++ code/03/Lesson3Five/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson3Five
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string input;
+
+            Console.Write("Enter a number: ");
+            input = Console.ReadLine();
+            x = int.Parse(input);
+                            
+            if (x > 5)
+            {
+                Console.WriteLine("Greater than 5");
+            }
+            else if (x < 5)
+            {
+                Console.WriteLine("Less than 5");
+            }
+            else
+            {
+                Console.WriteLine("Equals to 5");
+            } 
+        }
+    }
+}
